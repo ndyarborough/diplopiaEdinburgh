d324fb8366b833d2b6b96c02a88d5f99c3530b10
Nicholas <nicholasallenjr@gmail.com>
2017-10-10 19:52:26 -0400

    updated MVC model

---
+++ public/app.js
@@ -0,0 +1,94 @@
+const questionSet = {
+    q1: {
+        question: "Does the patient see two separate images or one blurred image?",
+        a1: true,
+        n1: "q2", 
+        a2: false,
+        n2: null,
+        previous: null
+    },
+    q2: {
+        question: "Is the patient's diplopia present with each eye covered separately?",
+        a1: true,
+        n1: "q3", 
+        a2: false,
+        n2: "q4",
+        previous: "q1"
+    },
+    q3: {
+        question: "Still present when patient looks throung a pinhole?",
+        a1: true,
+        n1: null, 
+        a2: false,
+        n2: null,
+        previous: "q2"
+    },
+    q4: {
+        question: "How are the two images separated??",
+        a1: true,
+        n1: "q5", 
+        a2: false,
+        n2: "q6",
+        previous: "q2"
+    },
+    q5: {
+        question: "Ask the patient to look to the side that makes the diplopia worse. Look at their eye alignment when they do this.",
+        a1: true,
+        n1: null, 
+        a2: false,
+        n2: null,
+        previous: "q4"
+    },
+    q6: {
+        question: "Are pupil sizes different (anisocoria) or is there ptosis present (lid drooping)?",
+        a1: true,
+        n1: null, 
+        a2: false,
+        n2: "q7",
+        previous: "q4"
+    },
+    q7: {
+        question: "Are there signs of thyroid eye disease? (proptosis/eye bulging or scleral baring)",
+        a1: true,
+        n1: null, 
+        a2: false,
+        n2: null,
+        previous: "q6"
+    }
+}
+
+var userInput;
+var currentQuestion = "q1";
+
+function exam() {
+    question1();
+}
+
+function question1() {
+    // Populate Question Div
+    var questionText = questionSet[currentQuestion].question;
+    var question = document.getElementById("question");
+    question.innerHTML = questionText;
+
+    // Populate Answers
+    var answer1 = document.getElementById("btn0");
+    var answer1Text = questionSet.q1.a1;
+    answer1.innerHTML = answer1Text;
+    var answer2 = document.getElementById("btn1");
+    var answer2Text = questionSet.q1.a2;
+    answer2.innerHTML = answer2Text;
+
+    $("#btn0").one("click", function() {
+        userInput = this.innerHTML;
+        currentQuestion = currentQuestion.n1;
+        question1();
+    });
+
+    $("#btn1").one("click", function() {
+        userInput = this.innerHTML;
+        question.innerHTML = "Blurred Vision <br> See visual loss algorithm (figure 3.2)";
+        currentQuestion = currentQuestion.n2;
+    });
+}
+
+exam();
---
+++ routes/html-routes.js
@@ -0,0 +1,15 @@
+var path = require("path");
+
+module.exports = function(app) {
+
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../index.html'));
+	})
+
+
+	app.get('/main', function(req, res) {
+		res.sendFile(path.join(__dirname, '../main.html'));
+	})
+
+
+}
