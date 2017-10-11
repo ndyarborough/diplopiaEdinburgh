74a908340b11b3880a9af083589e3558c9839061
ndyarborough <ndyarborough1997@gmail.com>
2017-10-11 18:58:26 -0400

    missed a file....

--- app.js
+++ app.js
@@ -1,101 +1,95 @@
-const blockTop = {
+const questionSet = {
     q1: {
         question: "Does the patient see two separate images or one blurred image?",
-        a1: true,
-        a2: false
-    }
-}
-
-const block1 = {
-    q1: {
+        a1: "Two separate images",
+        n1: "q2", 
+        a2: "One blurred image",
+        n2: null,
+        previous: null
+    },
+    q2: {
         question: "Is the patient's diplopia present with each eye covered separately?",
-        a1: true,
-        a2: false
+        a1: "Yes",
+        n1: "q3", 
+        a2: "No",
+        n2: "q4",
+        previous: "q1"
     },
-    q2: {
-        question: "Is the diplopia still present when the patient looks through a pinhole?",
-        a1: true,
-        a2: false
-    }
-}
-const block2 = {
-    q1: {
-        question: "How are the two images separated?",
-        a1: true, //Horizontally
-        a2: false //Vertically
+    q3: {
+        question: "Still present when patient looks throung a pinhole?",
+        a1: "Yes",
+        n1: "Cortical Abnormality", 
+        a2: "No",
+        n2: null,
+        previous: "q2"
     },
-    q2: {
+    q4: {
+        question: "How are the two images separated??",
+        a1: "Vertically",
+        n1: "q5", 
+        a2: "Horizontally",
+        n2: "q6",
+        previous: "q2"
+    },
+    q5: {
         question: "Ask the patient to look to the side that makes the diplopia worse. Look at their eye alignment when they do this.",
-        a1: true, //Eyes converge (come together)
-        a2: false //Eyes diverge (move apart)
-    }
-}
-const block3 = {
-    q1: {
-        question: "Are pupil sizes different (anisocoria) or is there ptosis present (lid drooping)?",
-        a1: true,
-        a2: false
+        a1: "Eyes Converge",
+        n1: null, 
+        a2: "Eyes Diverge",
+        n2: null,
+        previous: "q4"
     },
-    q2: {
+    q6: {
+        question: "Are pupil sizes different (anisocoria) or is there ptosis present (lid drooping)?",
+        a1: "Ptosis present",
+        n1: null, 
+        a2: "Ptosis absent",
+        n2: "q7",
+        previous: "q4"
+    },
+    q7: {
         question: "Are there signs of thyroid eye disease? (proptosis/eye bulging or scleral baring)",
-        a1: true,
-        a2: false
+        a1: "Yes",
+        n1: null, 
+        a2: "No",
+        n2: null,
+        previous: "q6"
     }
 }
 
 var userInput;
 
-function exam() {
-
-    // Everyone gets to question 1
-    question1();
-    // // If true     // If false
-    // Question2a();  Question2b();
-}
-
-function question1() {
+function exam(currentQuestion) {
     // Populate Question Div
-    var questionText = blockTop.q1.question;
+    var questionText = questionSet[currentQuestion].question;
     var question = document.getElementById("question");
     question.innerHTML = questionText;
 
     // Populate Answers
     var answer1 = document.getElementById("btn0");
-    var answer1Text = blockTop.q1.a1;
-    answer1.innerHTML = answer1Text;
     var answer2 = document.getElementById("btn1");
-    var answer2Text = blockTop.q1.a2;
+    var answer1Text = questionSet[currentQuestion].a1;
+    var answer2Text = questionSet[currentQuestion].a2;
+    answer1.innerHTML = answer1Text;
     answer2.innerHTML = answer2Text;
 
-    $("#btn0").one("click", function() {
-        userInput = this.innerHTML;
-        runBlock1();
-    });
+    $(".previousQuestion").on("click", function() {
+        $(".previousQuestion").off();
+        exam(questionSet[currentQuestion].previous)
+    })
 
-    $("#btn1").one("click", function() {
-        userInput = this.innerHTML;
-        question.innerHTML = "Blurred Vision <br> See visual loss algorithm (figure 3.2)";
-        $("#buttons").empty();
+    $(".btn").on("click", function() {
+        // Ensures no duplicate click handlers
+        $(".btn").off();
+        // If True is clicked
+        if ($(this).attr("id") === "btn0") {
+            exam(questionSet[currentQuestion].n1)
+        } 
+        // If False is clicked
+        else {
+            exam(questionSet[currentQuestion].n2)
+        }
     });
 }
 
-function runBlock1() {
-    // Populate Question Div
-    var questionText = block1.q1.question;
-    var question = document.getElementById("question");
-    question.innerHTML = questionText;
-
-    // Populate Answers
-    var answer1 = document.getElementById("btn0");
-    var answer1Text = block1.q1.a1;
-    answer1.innerHTML = answer1Text;
-    var answer2 = document.getElementById("btn1");
-    var answer2Text = block1.q1.a2;
-    answer2.innerHTML = answer2Text;
-}
-
-
-
-
-
-exam();
+exam("q1");
