30818eab2f6c24788edb6190e3c0f115e0f2c802
Nicholas Allen <nicholasallenjr@gmail.com>
2017-10-17 18:30:09 -0400

    updated jawsdb code

--- models/index.js
+++ models/index.js
@@ -8,11 +8,6 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
@@ -25,6 +20,11 @@ if (process.env.JAWSDB_URL) {
   });
 };
 
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
 
 fs
   .readdirSync(__dirname)
